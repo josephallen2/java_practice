7d3313ddf6e3a14bf46279b330be33a4edf62659
joseph <joseph_allen13@outlook.com>
2021-06-21 00:14:05 -0500

    work with integer streams

--- app/src/main/java/java_practice/App.java
+++ app/src/main/java/java_practice/App.java
@@ -11,15 +11,20 @@ public class App {
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-        String string = "this is a string";
-        
+
         //streams
+
+        //integer stream
         IntStream
-            .range(1,10)
+            .range(1, 10)
             .forEach(System.out::print);
         System.out.println();
+
+        IntStream
+            .range(1, 10)
+            .skip(5)
+            .forEach(x->System.out.println(x));
+        System.out.println();
         
-        string.chars().forEachOrdered(i -> System.out.print((char)i));
     }
 }
